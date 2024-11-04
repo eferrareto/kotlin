c6e8265cb19600f8734f010c7796d7facc713482
Erick Ferrareto <e.ferraretowork@gmail.com>
2024-11-03 23:02:16 -0300

    Commit feito pelo Intellj

--- OOP.kt
+++ OOP.kt
@@ -1,6 +1,21 @@
 fun main() {
+    var controle = Ps4Controller.Button()
+    var botao = Ps4Controller()
+    controle.press(botao.botao1)
+    controle.press(botao.botao4)
+    controle.press(botao.botao6)
+}
 
-
-
-
+class Ps4Controller{
+    val botao1 = "Triangulo"
+    val botao2 = "Circulo"
+    val botao3 = "Cruz"
+    val botao4 = "Quadrado"
+    val botao5 = "L1"
+    val botao6 = "R1"
+    class Button{
+        fun press(button: String){
+            println("${button} botão pressionado!")
+        }
+    }
 }
