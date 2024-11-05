30c95a01d6a0f2126f7cfca309c58c21543fc089
Erick Ferrareto <e.ferraretowork@gmail.com>
2024-11-04 23:28:35 -0300

    exercicios Inner Class

--- OOP.kt
+++ OOP.kt
@@ -1,9 +1,10 @@
 fun main() {
-    var controle = Ps4Controller.Button()
-    var botao = Ps4Controller()
-    controle.press(botao.botao1)
-    controle.press(botao.botao4)
-    controle.press(botao.botao6)
+    var lojinhDoErick = Loja()
+    var produto1 = lojinhDoErick.Produto("Camisa da Nike", 145.80, "Camisetas")
+    println("Produto: ${produto1.nome}")
+    println("Preço: ${produto1.preco}")
+    println("Categoria: ${produto1.categoria}")
+
 }
 
 class Ps4Controller{
@@ -17,5 +18,23 @@ class Ps4Controller{
         fun press(button: String){
             println("${button} botão pressionado!")
         }
+    }
+}
+class Universidade{
+    inner class Aluno(var nome : String, var matricula: Int, var curso : String){
+
+    }
+    fun matricularAluno(nome : String, matricula: Int, curso: String){
+        println("Aluno: ${nome} está matriculado, o seu numero é ${matricula}, e o seu curso é ${curso}")
+    }
+}
+class Loja{
+    inner class Produto(var nome : String, var preco : Double, var categoria : String){
+
+    }
+    fun adicionarProduto(nome: String, preco: Double, categoria: String){
+        println("Produto: $nome")
+        println("Preço: $preco")
+        println("Categoria: $categoria")
     }
 }
